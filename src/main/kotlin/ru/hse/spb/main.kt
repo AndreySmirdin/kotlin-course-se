ac82b987c6f7cee049b1a7afd51fb62e440a7f3e
andrey239 <andrey.smirdin@mail.ru>
2018-09-09 20:36:23 +0300

    Task 598C.

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,64 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.sign
 
-    return words.joinToString(separator = " ")
+
+class Vector(private val x: Long, private val y: Long, val id: Int) : Comparable<Vector> {
+    private val angle = Math.atan2(x.toDouble(), y.toDouble())
+
+    override fun compareTo(other: Vector): Int {
+        return angle.compareTo(other.angle)
+    }
+
+    fun pseudoscalarProduct(other: Vector) = x * other.y - y * other.x
+    fun scalarProduct(other: Vector) = x * other.x + y * other.y
 }
 
+class Angle(val v1: Vector, val v2: Vector) : Comparable<Angle> {
+    override fun compareTo(other: Angle): Int {
+        val scalar1 = v1.scalarProduct(v2)
+        val pseudo1 = -v1.pseudoscalarProduct(v2)
+        val scalar2 = other.v1.scalarProduct(other.v2)
+        val pseudo2 = -other.v1.pseudoscalarProduct(other.v2)
+        return (scalar1 * pseudo2 - scalar2 * pseudo1).sign
+    }
+}
+
+fun addAngle(v1: Vector, v2: Vector, angles: ArrayList<Angle>) {
+    val pseudo = v1.pseudoscalarProduct(v2)
+    angles.add(if (pseudo < 0) Angle(v1, v2) else Angle(v2, v1))
+}
+
+fun findMinAnglePair(vectors: ArrayList<Vector>): Pair<Int, Int> {
+    vectors.sort()
+
+    val angles = ArrayList<Angle>()
+    for (i in vectors.indices) {
+        if (i == vectors.size - 1)
+            continue
+        addAngle(vectors[i], vectors[i + 1], angles)
+    }
+    addAngle(vectors.first(), vectors.last(), angles)
+    val best = angles.max()
+    return Pair(best!!.v1.id, best.v2.id)
+}
+
+
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val input = Scanner(System.`in`)
+    val n = input.nextInt()
+    val vectors = ArrayList<Vector>(n)
+    var x: Long
+    var y: Long
+
+    for (i in 1..n) {
+        x = input.nextLong()
+        y = input.nextLong()
+        vectors.add(Vector(x, y, i))
+    }
+
+    val answer = findMinAnglePair(vectors)
+    println("${answer.first} ${answer.second}")
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,23 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.anyOf
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testFindMinAnglePair() {
+        var vectors = mutableListOf(Vector(1, 0, 3), Vector(0, 1, 2)) as ArrayList<Vector>
+        assertThat(findMinAnglePair(vectors), anyOf(`is`(Pair(3, 2)), `is`(Pair(2, 3))))
+
+        vectors = mutableListOf(Vector(-1, 0, 1),
+                Vector(0, -1, 2),
+                Vector(1, 0, 3),
+                Vector(1, 1, 4),
+                Vector(-4, -5, 5),
+                Vector(-4, -6, 6)) as ArrayList<Vector>
+        assertThat(findMinAnglePair(vectors), anyOf(`is`(Pair(5, 6)), `is`(Pair(6, 5))))
     }
 }
